a38f7877abf768196a0c68c9fdff230fced6621c
Kumaranvelan <kumaravelan9721@gmail.com>
2023-09-21 16:44:37 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -35,10 +35,31 @@ const App = () => {
     criticalaccount: [],
     payment: [],
   });
-const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+
+  console.log(persons);
+
+  const [editIndex, setEditIndex] = useState<number | null>(null);
+  const [editFormData, setEditFormData] = useState<Person | null>(null);
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>,
+    field: string
+  ) => {
     const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
+    if (editIndex !== null) {
+      // If in edit mode, update the editFormData
+      setEditFormData((prevData) => {
+        return {
+          ...(prevData as Person),
+          [field]: value,
+        };
+      });
+    } else {
+      // Otherwise, update the regular formData
+      setFormData({ ...formData, [name]: value });
+    }
   };
+  
 const AddPerson = (event: React.FormEvent) => {
     event.preventDefault();
     setPersons((prevPersons) => [...prevPersons, formData]);
@@ -62,61 +83,63 @@ const AddPerson = (event: React.FormEvent) => {
     });
   };
 
-
-
-
-
-  console.log(persons);
-
-  // In your App component
-
-const handleDelete = (index: number) => {
-  // Show a confirmation dialog to confirm the delete action
+  const handleDelete = (index: number) => {
   const confirmDelete = window.confirm('Are you sure you want to delete this person?');
 
   if (confirmDelete) {
-    // Create a copy of the persons array
     const updatedPersons = [...persons];
-
-    // Remove the selected person from the copied array
     updatedPersons.splice(index, 1);
-
-    // Update the state with the updated array
     setPersons(updatedPersons);
   }
 };
 
+const handleEdit = (index: number) => {
+  
+  setEditIndex(index);
+  setEditFormData(persons[index]);
+};
 
-
-
+const handleEditSubmit = (event:React.FormEvent) => {
+  event.preventDefault();
+  if (editIndex !== null && editFormData !== null) {
+    // Create a copy of the persons array
+    const updatedPersons = [...persons];
+    // Update the person's data in the copied array
+    updatedPersons[editIndex] = editFormData;
+// Update the state with the updated array and reset edit mode
+    setPersons(updatedPersons);
+    setEditIndex(null);
+    setEditFormData(null);
+  }
+};
    return (
     <div>
     <form id="myForm" >
     <label htmlFor="name">Name</label>
-    <input type="text" id="name" name="name" value={formData.name} onChange={handleInputChange} />
+    <input type="text" id="name" name="name" value={editIndex !== null ? editFormData?.name:formData.name} onChange={(event) => handleInputChange(event, 'name')} />
     <label>Email</label>
-    <input type="text" name="email" id="email" value={formData.email} onChange={handleInputChange} />
+    <input type="text" name="email" id="email" value={editIndex !== null ? editFormData?.email:formData.email} onChange={(event) => handleInputChange(event, 'email')} />
     <label>PhoneNumber</label>
-    <input type="tel" name="number" id="number" value={formData.phone} onChange={handleInputChange}/>
+    <input type="tel" name="number" id="number" value={editIndex !== null ? editFormData?.phone:formData.phone} onChange={(event) => handleInputChange(event, 'phone')}/>
     <label>website</label>
-    <input type="text" id="website" name="website" value={formData.website} onChange={handleInputChange} />
+    <input type="text" id="website" name="website" value={editIndex !== null ? editFormData?.website:formData.website} onChange={(event) => handleInputChange(event, 'website')} />
     <label >contactname</label>
-    <input type="text" id="contact" name="contact"value={formData.contact} onChange={handleInputChange}/>
+    <input type="text" id="contact" name="contact"value={editIndex !== null ? editFormData?.contact:formData.contact} onChange={(event) => handleInputChange(event, 'contact')}/>
     <label >contactnumber</label>
-    <input type="tel" id="phone" name="phone"value={formData.number} onChange={handleInputChange} />
+    <input type="tel" id="phone" name="phone"value={editIndex !== null ? editFormData?.number :formData.number} onChange={(event) => handleInputChange(event, 'number')} />
     <label>ContactMail</label>
-    <input type="email" id="another" name="another"value={formData.another} onChange={handleInputChange}/>
+    <input type="email" id="another" name="another"value={editIndex !== null ? editFormData?.another :formData.another} onChange={(event) => handleInputChange(event, 'another')}/>
     <label>Notes</label>
-    <textarea name="notes" id="notes" value={formData.notes} onChange={handleInputChange}></textarea>
+    <textarea name="notes" id="notes" value={editIndex !== null ? editFormData?.notes:formData.notes} onChange={(event) => handleInputChange(event, 'notes')}></textarea>
     <label >Type:</label>
     <label >Small buisiness</label>
-    <input type="radio" name="type" id="small business" value="small business" checked={formData.type.includes('small business')} onChange={handleInputChange}/>
+    <input type="radio" name="type" id="small business" value={editIndex !== null ? editFormData?.type:"small business"}onChange={(event) => handleInputChange(event, 'small business')}/>
     <label >Entreprise</label>
-    <input type="radio" name="type" id="entreprise" value="Entreprise" checked={formData.type.includes('Entreprise')} onChange={handleInputChange}/>
+    <input type="radio" name="type" id="entreprise" value={editIndex !== null ? editFormData?.type:"Entreprise"} onChange={(event) => handleInputChange(event, 'Entreprise')}/>
     <label >Entreprenuer</label>
-    <input type="radio" name="type" id="entreprenuer" value="Entreprenuer" checked={formData.type.includes('Entreprenuer')} onChange={handleInputChange}/>
+    <input type="radio" name="type" id="entreprenuer" value={editIndex !== null ? editFormData?.type:"Entreprenuer"} onChange={(event) => handleInputChange(event, 'Entreprenuer')}/>
     <label > category</label>
-    <select name="category" id="category" value={formData.category} onChange={handleInputChange}>
+    <select name="category" id="category" value={editIndex !== null ? editFormData?.category:formData.category} onChange={(event) => handleInputChange(event, 'category')}>
         <option value="clothes">clothes</option>
         <option value="toys">toys</option>
         <option value="electronics">Electronics</option>
@@ -124,22 +147,23 @@ const handleDelete = (index: number) => {
         <option value="digital">Digital</option>
     </select>
     <label >commmission</label>
-    <input type="tel" name="commission" id="commission" value={formData.commission} onChange={handleInputChange}/>
+    <input type="tel" name="commission" id="commission" value={editIndex !== null ? editFormData?.commission:formData.commission} onChange={(event) => handleInputChange(event, 'commission')}/>
     <label >Activeform</label>
-    <input type="date" name="date" id="date" value={formData.date} onChange={handleInputChange}/>
+    <input type="date" name="date" id="date" value={editIndex !== null ? editFormData?.date:formData.date} onChange={(event) => handleInputChange(event, 'date')}/>
     <label>LOGO</label>
-    <input type="file" name="file" id="file" value={formData.logo} onChange={handleInputChange} />
+    <input type="file" name="file" id="file" value={editIndex !== null ? editFormData?.logo:formData.logo} onChange={(event) => handleInputChange(event, 'logo')} />
     <label >Critical Account</label>
     <label>YES</label>
-    <input type="checkbox" name="criticalaccount" id="yes" value="Yes" checked={formData.criticalaccount.includes('Yes')} onChange={handleInputChange}  />
+    <input type="checkbox" name="criticalaccount" id="yes" value={editIndex !== null ? editFormData?.criticalaccount:"Yes"}  onChange={(event) => handleInputChange(event, 'Yes')}  />
     <label>NO</label>
-    <input type="checkbox" name="criticalaccount" id="no" value="No" checked={formData.criticalaccount.includes('No')} onChange={handleInputChange}/>
-    <select name="payment" id="payment" value={formData.payment} onChange={handleInputChange}>
+    <input type="checkbox" name="criticalaccount" id="no" value={editIndex !== null ? editFormData?.criticalaccount:"No"} onChange={(event) => handleInputChange(event, 'No')}/>
+    <select name="payment" id="payment" value={editIndex !== null ? editFormData?.payment:formData.payment} onChange={(event) => handleInputChange(event, 'payment')}>
         <option value="cash">Cash on Payment</option>
         <option value="upi">UPI</option>
         <option value="card">Card on payment</option>
     </select>
-    <input type="submit" value="Submit" onClick={AddPerson}/> 
+    <input type="submit" value={editIndex !== null ? "Edit" :"Submit"} 
+     onClick={editIndex !== null ? handleEditSubmit : AddPerson}/> 
 </form> 
 
 <table className="App-table">
@@ -160,8 +184,6 @@ const handleDelete = (index: number) => {
             <th>criticalaccount</th>
             <th>Payment</th>
             <th>Action</th>
-          
-
             </tr>
             </thead>
             <tbody>
@@ -180,6 +202,7 @@ const handleDelete = (index: number) => {
                 <td>{p.date}</td>
                 <td>{p.criticalaccount}</td>
                 <td>{p.payment}</td>
+                <td><button onClick ={() =>handleEdit(index)}>Edit</button></td>
                 <td><button onClick={()=>handleDelete(index)}>Delete</button></td>
                </tr>)}
             </tbody>
@@ -187,5 +210,4 @@ const handleDelete = (index: number) => {
     </div>
   );
 }
-
 export default App;
